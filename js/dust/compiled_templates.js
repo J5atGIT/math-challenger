fc59e9cb2db7110a1ee4260f5a4e6d6cb3f7749e
Nkomo Butts <nbutts@Lacooda.local>
2013-03-15 11:06:20 -0400

    new udate

--- js/dust/compiled_templates.js
+++ js/dust/compiled_templates.js
@@ -2,7 +2,7 @@
   dust.register("demo", body_0);
 
   function body_0(chk, ctx) {
-    return chk.write("<span id=\"content-area-span\">").reference(ctx.get("text"), ctx, "h").write("</span><form id=\"htmlFormAnswerPanel\" onSubmit=\"oMM.next( this ); return false;\"><input type=\"text\" id=\"answerField\" name=\"answerField\" autocomplete=\"off\" title=\"Enter a NUMERIC answer only (Characters of 0 - 9 only)\" pattern=\"[0-9]{1,3}\" /></form>");
+    return chk.write("<span id=\"content-area-span\">").reference(ctx.get("text"), ctx, "h").write("</span><form id=\"htmlFormAnswerPanel\" onSubmit=\"oMM.next( this ); return false;\"><input type=\"number\" id=\"answerField\" name=\"answerField\" autocomplete=\"off\" title=\"Enter a NUMERIC answer only (Characters of 0 - 9 only)\" pattern=\"[0-9]{1,3}\" /></form>");
   }
   return body_0;
 })();
